ca49f8c7d9ed6972cabd04210d5b812e7d0f7a49
janhase88 <46456607+janhase88@users.noreply.github.com>
2025-09-12 03:53:20 +0200

    Make Skia and NanoVG font caches instance specific

--- IGraphics/Drawing/IGraphicsNanoVG.h
+++ IGraphics/Drawing/IGraphicsNanoVG.h
@@ -10,11 +10,11 @@
 
 #pragma once
 
-#include "IPlugPlatform.h"
 #include "IGraphics.h"
+#include "IPlugPlatform.h"
 
-#include "nanovg.h"
 #include "mutex.h"
+#include "nanovg.h"
 #include <stack>
 
 // Thanks to Olli Wang/MOUI for much of this macro magic  https://github.com/ollix/moui
@@ -56,9 +56,9 @@
   #define nvgBindFramebuffer(fb) nvgluBindFramebuffer(fb)
   #define nvgCreateFramebuffer(ctx, w, h, flags) nvgluCreateFramebuffer(ctx, w, h, flags)
   #define nvgDeleteFramebuffer(fb) nvgluDeleteFramebuffer(fb)
-  using NVGframebuffer = NVGLUframebuffer;
+using NVGframebuffer = NVGLUframebuffer;
 #elif defined IGRAPHICS_METAL
-  using NVGframebuffer = MNVGframebuffer;
+using NVGframebuffer = MNVGframebuffer;
 #endif
 
 BEGIN_IPLUG_NAMESPACE
@@ -76,13 +76,13 @@ void NanoVGSetBlendMode(NVGcontext* pContext, const IBlend* pBlend);
 /** Converts IPattern to NVGpaint */
 NVGpaint NanoVGPaint(NVGcontext* pContext, const IPattern& pattern, const IBlend* pBlend = 0);
 
-/** IGraphics draw class using NanoVG  
-*   @ingroup DrawClasses */
+/** IGraphics draw class using NanoVG
+ *   @ingroup DrawClasses */
 class IGraphicsNanoVG : public IGraphics
 {
 private:
   class Bitmap;
-  
+
 public:
   IGraphicsNanoVG(IGEditorDelegate& dlg, int w, int h, int fps, float scale);
   ~IGraphicsNanoVG();
@@ -101,9 +101,9 @@ public:
   void DrawDottedRect(const IColor& color, const IRECT& bounds, const IBlend* pBlend, float thickness, float dashLen) override;
 
   void DrawFastDropShadow(const IRECT& innerBounds, const IRECT& outerBounds, float xyDrop = 5.f, float roundness = 0.f, float blur = 10.f, IBlend* pBlend = nullptr) override;
-  
+
   void DrawMultiLineText(const IText& text, const char* str, const IRECT& bounds, const IBlend* pBlend) override;
-  
+
   void PathClear() override;
   void PathClose() override;
   void PathArc(float cx, float cy, float r, float a1, float a2, EWinding winding) override;
@@ -114,17 +114,17 @@ public:
   void PathSetWinding(bool clockwise) override;
   void PathStroke(const IPattern& pattern, float thickness, const IStrokeOptions& options, const IBlend* pBlend) override;
   void PathFill(const IPattern& pattern, const IFillOptions& options, const IBlend* pBlend) override;
-  
+
   IColor GetPoint(int x, int y) override;
-  void* GetDrawContext() override { return (void*) mVG; }
-    
+  void* GetDrawContext() override { return (void*)mVG; }
+
   IBitmap LoadBitmap(const char* name, int nStates, bool framesAreHorizontal, int targetScale) override;
-  void ReleaseBitmap(const IBitmap& bitmap) override { }; // NO-OP
-  void RetainBitmap(const IBitmap& bitmap, const char * cacheName) override { }; // NO-OP
+  void ReleaseBitmap(const IBitmap& bitmap) override {};                       // NO-OP
+  void RetainBitmap(const IBitmap& bitmap, const char* cacheName) override {}; // NO-OP
   bool BitmapExtSupported(const char* ext) override;
 
   void DeleteFBO(NVGframebuffer* pBuffer);
-  
+
 protected:
   APIBitmap* LoadAPIBitmap(const char* fileNameOrResID, int scale, EResourceLocation location, const char* ext) override;
   APIBitmap* LoadAPIBitmap(const char* name, const void* pData, int dataSize, int scale) override;
@@ -133,7 +133,7 @@ protected:
   bool LoadAPIFont(const char* fontID, const PlatformFontPtr& font) override;
 
   int AlphaChannel() const override { return 3; }
-  
+
   bool FlippedBitmap() const override
   {
 #if defined(IGRAPHICS_GL)
@@ -150,16 +150,17 @@ protected:
   void DoDrawText(const IText& text, const char* str, const IRECT& bounds, const IBlend* pBlend) override;
 
 private:
-  void PrepareAndMeasureText(const IText& text, const char* str, IRECT& r, double& x, double & y) const;
+  void PrepareAndMeasureText(const IText& text, const char* str, IRECT& r, double& x, double& y) const;
   void PathTransformSetMatrix(const IMatrix& m) override;
   void SetClipRegion(const IRECT& r) override;
   void UpdateLayer() override;
   void ClearFBOStack();
-  
+
   bool mInDraw = false;
   WDL_Mutex mFBOMutex;
   std::stack<NVGframebuffer*> mFBOStack; // A stack of FBOs that requires freeing at the end of the frame
-  StaticStorage<APIBitmap> mBitmapCache; //not actually static (doesn't require retaining or releasing)
+  StaticStorage<APIBitmap> mBitmapCache; // not actually static (doesn't require retaining or releasing)
+  StaticStorage<IFontData> mFontCache;   // per-instance font cache
   NVGcontext* mVG = nullptr;
   NVGframebuffer* mMainFrameBuffer = nullptr;
   int mInitialFBO = 0;

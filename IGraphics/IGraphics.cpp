32462d40010ca9977341a7f3f754a469a25c2e5e
janhase88 <46456607+janhase88@users.noreply.github.com>
2025-09-13 07:15:38 +0200

    Use control tag in trace

--- IGraphics/IGraphics.cpp
+++ IGraphics/IGraphics.cpp
@@ -969,11 +969,10 @@ void IGraphics::DrawControl(IControl* pControl, const IRECT& bounds, float scale
     auto* plug = dynamic_cast<IPluginBase*>(GetDelegate());
     if (plug)
     {
-      WDL_String label{pControl->GetName()};
-      label.AppendFormatted(64, ":%d", pControl->GetTag());
       if (CStringHasContents(pControl->GetGroup()))
-        label.AppendFormatted(64, ":%s", pControl->GetGroup());
-      TRACE_SCOPE_F(plug->GetLogFile(), label.Get());
+        Trace(plug->GetLogFile(), TRACELOC, "tag=%d:%s", pControl->GetTag(), pControl->GetGroup());
+      else
+        Trace(plug->GetLogFile(), TRACELOC, "tag=%d", pControl->GetTag());
     }
     pControl->Draw(*this);
 #ifdef AAX_API

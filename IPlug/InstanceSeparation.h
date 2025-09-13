486a17a282f54fec9e325a12ee499d0972a90ddc
janhase88 <46456607+janhase88@users.noreply.github.com>
2025-09-13 22:39:40 +0200

    Allow optional separation of graphics caches (#133)

---
+++ IPlug/InstanceSeparation.h
@@ -0,0 +1,23 @@
+/*
+ ===============================================================================
+
+ This file is part of the iPlug 2 library.
+
+ See LICENSE.txt for  more info.
+
+ ===============================================================================
+*/
+
+#pragma once
+
+// Macros controlling whether certain caches are separated per IGraphics instance
+// rather than shared across all instances.
+
+#ifndef IPLUG_SEPARATE_BITMAP_CACHE
+#define IPLUG_SEPARATE_BITMAP_CACHE 0
+#endif
+
+#ifndef IPLUG_SEPARATE_SVG_CACHE
+#define IPLUG_SEPARATE_SVG_CACHE 0
+#endif
+

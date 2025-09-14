84cd2e4996c911685207a51bfe0a8ce38b012ad4
janhase88 <46456607+janhase88@users.noreply.github.com>
2025-09-14 04:03:40 +0200

    Fix GLEW header order and GLAD detection (#163)

--- WDL/lice/lice_gl_ctx.h
+++ WDL/lice/lice_gl_ctx.h
@@ -1,21 +1,18 @@
 #ifndef _GL_CTX_
 #define _GL_CTX_
 
-#include "lice.h"
-#include "../../IPlug/InstanceSeparation.h"
-
-
-#ifdef GLAD_GL_H
+#if defined(GLAD_GL_H) || defined(IGRAPHICS_GL2) || defined(IGRAPHICS_GL3)
 #include <glad/glad.h>
-
 #include <GL/glu.h>
 #else
 #define GLEW_STATIC
 #include "glew/include/GL/glew.h"
 #include "glew/include/GL/wglew.h"
-#include "glew/include/GL/wglext.h"
-
+#include "glew/include/GL/WGLEXT.H"
 #endif
+
+#include "lice.h"
+#include "../../IPlug/InstanceSeparation.h"
 
 #define MAX_CACHED_GLYPHS 4096
 

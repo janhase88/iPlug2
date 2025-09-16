17482b06f2ed3aba08cc103237ab893923902537
janhase88 <46456607+janhase88@users.noreply.github.com>
2025-09-16 02:23:18 +0200

    Define NOGDI for VST3 builds

--- IPlug/VST3/IPlugVST3.h
+++ IPlug/VST3/IPlugVST3.h
@@ -12,6 +12,10 @@
 #define _IPLUGAPI_
 // Only load one API class!
 
+#if defined(_WIN32) && !defined(NOGDI)
+  #define NOGDI // stop <windows.h> from declaring LOGFONT
+#endif
+
 /**
  * @file
  * @copydoc IPlugVST3

9639174a88f327b8c8e9248cd454411aadb21728
janhase88 <46456607+janhase88@users.noreply.github.com>
2025-09-22 02:24:01 +0200

    Fix mouse capture release order on Windows

--- IGraphics/Platforms/IGraphicsWin.cpp
+++ IGraphics/Platforms/IGraphicsWin.cpp
@@ -518,10 +518,14 @@ LRESULT CALLBACK IGraphicsWin::WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARA
   }
   case WM_LBUTTONUP:
   case WM_RBUTTONUP: {
-    ReleaseCapture();
+    const bool hadCapture = (GetCapture() == hWnd);
     IMouseInfo info = pGraphics->GetMouseInfo(lParam, wParam);
     std::vector<IMouseInfo> list{info};
     pGraphics->OnMouseUp(list);
+    if (hadCapture && GetCapture() == hWnd)
+    {
+      ReleaseCapture();
+    }
     return 0;
   }
   case WM_LBUTTONDBLCLK:
